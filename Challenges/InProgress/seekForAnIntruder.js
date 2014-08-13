160f01564bb1ef6d88a365b12c2d7dcad42458fd
Steven Mirecki <steven.mirecki@gmail.com>
2014-08-12 21:16:41 -0400

    updated notes
    updated notes

--- Challenges/InProgress/seekForAnIntruder.js
+++ Challenges/InProgress/seekForAnIntruder.js
@@ -12,3 +12,27 @@ fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) 
          }
     }
 });
+
+=====================
+
+// http://jsfiddle.net/bvyt4qst/
+// http://jsfiddle.net/7q00dmuu/
+
+var line = new String('255.255.255.255 ladkfja;oiavkn ');
+
+var re = /\B(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\B/;
+
+var re2 = /d(b+)(d)/ig;
+
+var m = re.exec(line);
+
+if (m === null) {
+    alert("No match");
+} else {
+    var s = "Match at position " + m.index + ":\n";
+    for (i = 0; i < m.length; i++) {
+        s = s + m[i] + "\n";
+    }
+    console.log(s);
+    alert(s);
+}
