53db664da8e4d8f02021eba47a0b983e264d9c29
Steven Mirecki <steven.mirecki@gmail.com>
2014-09-17 23:19:17 -0400

    update
    update

--- Challenges/InProgress/Decrypt_string.py
+++ Challenges/InProgress/Decrypt_string.py
@@ -7,6 +7,9 @@ for index in range(len(test)):
     test_dict[index] = test[index]  
   
 print test_dict
+#Executing the program....
+$python2.7 main.py
+{0: 'B', 1: 'H', 2: 'I', 3: 'S', 4: 'O', 5: 'E', 6: 'C', 7: 'R', 8: 'T', 9: 'M', 10: 'G', 11: 'W', 12: 'Y', 13: 'V', 14: 'A', 15: 'L', 16: 'U', 17: 'Z', 18: 'D', 19: 'N', 20: 'F', 21: 'J', 22: 'K', 23: 'P', 24: 'Q', 25: 'X'}
 message = "012222"
 
 for index in range(len(test_dict)):
@@ -23,6 +26,14 @@ for index in range(len(fruits)):
 print "Good bye!"
 
 message: "012222 1114142503 0313012513 03141418192102 0113 2419182119021713 06131715070119",
+# 012222
+# 1114142503
+# 0313012513
+# 03141418192101
+# 0113 - 0,1,1,3/BHHI 0,11,3/BWS 0,1,13/BHV ?????
+# 2419182119021713
+# 06131715070119
+
 keyed_alphabet: "BHISOECRTMGWYVALUZDNFJKPQX"
 
 message = "012222 1114142503 0313012513 03141418192102 0113 2419182119021713 06131715070119",
