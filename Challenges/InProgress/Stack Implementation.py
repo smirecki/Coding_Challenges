d14bb36e059b6b8caf3745a2109f64310f18e2c4
Steven Mirecki <steven.mirecki@gmail.com>
2014-09-17 21:32:49 -0400

    in progress notes
    notes on stack implementation

--- Challenges/InProgress/Stack Implementation.py
+++ Challenges/InProgress/Stack Implementation.py
@@ -119,5 +119,14 @@ post test
 -----------------OUTPUT END -----------------------------
 
 
-
-"""
+------------------Soultion
+""" 
+import sys
+test_cases = open(sys.argv[1], 'r')		# open file and read test file 
+for test in test_cases:					# read one line at a time
+    test = test.split( )				# split the string into a list (space as the default delimiter and removing /n newlines)
+    test.reverse()						# reverse the order of the list items
+    test = test[::2]					# get every other item
+    test = ' '.join(test)				# convert the list to a string seperated by ' ' spaces
+    print test_cases					# print the computed string
+test_cases.close()						# close file
