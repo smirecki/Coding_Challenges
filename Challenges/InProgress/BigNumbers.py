bce7f27ecedb58c7830363a106292c0b64abdbc2
Steven Mirecki <steven.mirecki@gmail.com>
2014-09-27 21:53:01 -0400

    Recode in Python
    Converting to Python - error with indent

---
+++ Challenges/InProgress/BigNumbers.py
@@ -0,0 +1,90 @@
+#Codeval - Big Numbers
+
+import sys
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+	# 'test' represents the test case, do something with it
+    line1 = ""
+    line2 = ""
+    line3 = ""
+    line4 = ""
+    line5 = ""
+    line6 = ""
+
+    for element in test:
+		if element == "0":
+            line1 += "-**--"
+            line2 += "*--*-"
+            line3 += "*--*-"
+            line4 += "*--*-"
+            line5 += "-**--"
+            line6 += "-----"
+        elif element == "1":
+            line1 += "--*--"
+            line2 += "-**--"
+            line3 += "--*--"
+            line4 += "--*--"
+            line5 += "-***-"
+            line6 += "-----"
+        elif element == "2":
+            line1 += "***--"
+            line2 += "---*-"
+            line3 += "-**--"
+            line4 += "*----"
+            line5 += "****-"
+            line6 += "-----"
+		elif element == "3":   	
+            line1 += "***--"
+            line2 += "---*-"
+            line3 += "-**--"
+            line4 += "---*-"
+            line5 += "***--"
+            line6 += "-----"
+		elif element == "4":    	
+            line1 += "-*---"
+            line2 += "*--*-"
+            line3 += "****-"
+            line4 += "---*-"
+            line5 += "---*-"
+            line6 += "-----"
+		elif element == "5":    	
+            line1 += "****-"
+            line2 += "*----"
+            line3 += "***--"
+            line4 += "---*-"
+            line5 += "***--"
+            line6 += "-----"
+		elif element == "6":    	
+            line1 += "-**--"
+            line2 += "*----"
+            line3 += "***--"
+            line4 += "*--*-"
+            line5 += "-**--"
+            line6 += "-----"
+		elif element == "7":    	
+            line1 += "****-"
+            line2 += "---*-"
+            line3 += "--*--"
+            line4 += "-*---"
+            line5 += "-*---"
+            line6 += "-----"
+		elif element == "8":    	
+            line1 += "-**--"
+            line2 += "*--*-"
+            line3 += "-**--"
+            line4 += "*--*-"
+            line5 += "-**--"
+            line6 += "-----"
+        elif element == "9":
+            line1 += "-**--"
+            line2 += "*--*-"
+            line3 += "-***-"
+            line4 += "---*-"
+            line5 += "-**--"
+            line6 += "-----"
+        else:
+
+	print line1, line2, line3, line4, line5, line6
+
+
+test_cases.close()
